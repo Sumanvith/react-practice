5ebda8a01e848ca83674e3f4bc5c07ff45484a6c
Sumanvith <dellbilla@gmail.com>
2024-07-29 21:14:38 +0530

    Body Seach Bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addName } from "../utils/nameSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
+import { cacheResults } from "../utils/searchSlice";
 
 const Body = () => {
   const [input, setInput] = useState("");
   const [message, setMessage] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
   const dispatch = useDispatch();
+  const searchCache = useSelector((store) => store.search);
   const names = useSelector((state) => state.name);
 
   const handleInput = () => {
@@ -18,19 +24,67 @@ const Body = () => {
     setInput("");
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSugsestions();
+      }
+    }, 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSugsestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+    dispatch(
+      cacheResults({
+        [searchQuery]: json[1],
+      })
+    );
+  };
   return (
     <div className="text-center">
-      <span>Enter text </span>
-      <input
-        className="p-2 m-4 border border-black h-10 w-24"
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button className="border border-black w-10" onClick={handleInput}>
-        Add
-      </button>
-      {message && <h1 className="text-red-500">{message}</h1>}
+      <div>
+        <span>Enter text </span>
+        <input
+          className="p-2 m-4 border border-black h-10 w-24"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="border border-black w-10" onClick={handleInput}>
+          Add
+        </button>
+        {message && <h1 className="text-red-500">{message}</h1>}
+      </div>
+      <div className="flex justify-center">
+        <input
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="px-5 w-1/3 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+          onBlur={() => setShowSuggestions(false)}
+          onFocus={() => setShowSuggestions(true)}></input>
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+          🔍
+        </button>
+        {showSuggestions && searchQuery && (
+          <div className="absolute bg-white py-2 px-2 m-12 w-1/3 shadow-lg rounded-lg border border-gray-100">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import nameSlice from "./nameSlice";
 import todoSlice from "./todoSlice";
+import searchSlice from "./searchSlice";
 
 const store = configureStore({
   reducer: {
     name: nameSlice,
     todo: todoSlice,
+    search: searchSlice,
   },
 });
 
